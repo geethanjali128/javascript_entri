5f7f838332e992eceaafcf82d07eee239acf58a4
geethanjali128 <karrageethanjali2003@gmail.com>
2023-09-23 19:28:56 +0530

    javascript

---
+++ test.js
@@ -0,0 +1,34 @@
+console.log("geethanjali")
+
+let x=2
+let y=3
+let z=x+y
+console.log(z)
+
+let fname="geethanjali"
+let lname="karra"
+console.log(fname+" "+lname )
+
+console.log(typeof(fname+" "+lname))
+
+let data="i have a car";console.log(data)
+
+let data1='i have a car';console.log(data1)
+
+let data2="it's my car"
+console.log(data2)
+
+let data3='this "car" is mine'
+console.log(data3)
+
+let data4="it'my \"new\" car"
+console.log(data4)
+
+let data5='it\'my "new"car'
+console.log(data5)
+
+let data6=`it' my "new" car`
+console.log(data6)
+
+let name="geethanajli"
+console.log(name.length)
