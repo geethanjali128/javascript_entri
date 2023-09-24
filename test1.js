f0ec3b992f03eaae9c2f143a1663b266cc0e3b18
geethanjali128 <karrageethanjali2003@gmail.com>
2023-09-24 19:45:17 +0530

    javascript1

---
+++ test1.js
@@ -0,0 +1,47 @@
+let x
+let y
+let z
+x=6
+y=7
+z=x+y
+console.log(z)
+
+let a=12
+let b=16
+let c=a+b
+console.log( typeof(c))
+
+let name="geethanjali"
+console.log(name.length)
+console.log(typeof(name))
+
+let fname="geethanjali"
+let lname="karra"
+let fullName=fname+" "+lname
+console.log(fname+" "+lname)
+console.log("fullName:",fullName)
+
+
+let data=6
+let data1="karra"
+let data2=7
+console.log(data+data1+data2)
+console.log(typeof(data+data1+data2))
+
+
+ fname="GEETHANJALI"
+ lname="KARRA"
+let text=fname.concat(lname).toLowerCase()
+console.log(text)
+
+
+
+
+
+
+
+
+
+
+
+
