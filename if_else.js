b7a93f842a5df200b19f126352a7df98ea636b4b
geethanjali128 <karrageethanjali2003@gmail.com>
2023-09-29 17:01:55 +0530

    javascript7

---
+++ if_else.js
@@ -0,0 +1,39 @@
+// if 
+let greeting=""
+ let hour=10
+if(hour<18){
+    greeting="good day"
+}
+console.log(greeting)
+
+// if else 
+let greeting1=""
+ let hour1=20
+if(hour1<18){
+    greeting1="good day"
+}
+else{
+    greeting1="good evening"
+}
+console.log(greeting1)
+
+// else if 
+let greeting2=""
+ let hour2=9
+if(hour2<10){
+    greeting2="good morning"
+}else if(hour2<20){
+    greeting2="good day"
+}else{
+    greeting2="good evening"
+}
+
+console.log(greeting2)
+ 
+let show=""
+let a=1
+if (a%2==0){
+   show="even number"
+}else{
+    show="odd number"
+}console.log(show)
