d78529fcd0cdf93500d31dec660822decd52755e
geethanjali128 <karrageethanjali2003@gmail.com>
2023-09-29 17:04:54 +0530

    javascript8

---
+++ if_else1.js
@@ -0,0 +1,50 @@
+let num1=15
+let num2=10
+total=""
+if(!num1<num2){
+    total=num1+num2
+}
+console.log(total)
+
+let num3=4
+let num4=8
+total1=""
+let isStudent=true
+if(isStudent){
+    total1=num3-num4
+}
+console.log(total1)
+
+let num5=12
+let num6=8
+total1=""
+let isStudent1=false
+if(!isStudent1){
+    total2=num5-num6
+}
+console.log(total2)
+ 
+let show=""
+let rollNo=5
+if(rollNo==1){
+    show="rajesh"
+}else if(rollNo==2){
+    show="suresh"
+}else if(rollNo==3){
+    show="yamini"
+}else if(rollNo==4){
+    show="aparna"
+}else if(rollNo==5){
+    show="maggy"
+}else if(rollNo==6){
+    show="sarath"
+}else if(rollNo==7){
+    show="divya"
+}else if(rollNo==8){
+    show="lavanya"
+}else if(rollNo==9){
+    show="kavya"
+}else{
+    show="None"
+}
+console.log(show)
