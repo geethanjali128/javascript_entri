64c6d13d6dd931e63c877cee3c7266d87c6a5e05
geethanjali128 <karrageethanjali2003@gmail.com>
2023-09-30 16:12:37 +0530

    if else

---
+++ if_else2.js
@@ -0,0 +1,42 @@
+let a=5
+console.log(a)
+a+=5
+console.log(a)
+a*=2
+console.log(a)
+a/=5
+console.log(a)
+
+
+let show=""
+let time="am"
+if(time=="am"){
+    show="good morning"
+}else if(time=="pm"){
+    show="good afternoon"
+}else{
+    show="good evening"
+}
+
+console.log(show)
+
+
+
+let total_students
+let batchNo=10
+if(batchNo==1){
+    total_students=50
+}else if(batchNo==2){
+    total_students=40
+}else if(batchNo==3){
+    total_students=55
+}else if(batchNo==4){
+    total_students=35
+}else if(batchNo==5){
+    total_students=60
+}else if(batchNo==6){
+    total_students=75
+}else{
+    total_students="None"
+}
+console.log(total_students)
