fb05b5cce21c2f61a8358c64e288200ef6255c39
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-01 18:57:02 +0530

    indexof

---
+++ indexof.js
@@ -0,0 +1,17 @@
+// indexOf()
+const text="Please 'locate' where locate occurs!"
+const index=text.indexOf("locate")
+console.log(index)
+
+// // lastIndexOf()
+const text1="Please 'locate' where locate occurs!"
+const index1=text1.lastIndexOf("locate")
+console.log(index1)
+
+// index starts from "0"
+const data="i'm from avanigadda,avanigadda."
+const index2=data.indexOf("avanigadda")
+console.log(index2)
+const index3=data.lastIndexOf("avanigadda")
+console.log(index3)
+
