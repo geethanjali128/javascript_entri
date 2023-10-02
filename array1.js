4a557a346a31c3460c1e68016560cf250c0975b1
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-02 17:30:16 +0530

    array

---
+++ array1.js
@@ -0,0 +1,31 @@
+const foodItems=["biryani","chicken fry","mutton soup","mushroom curry"]
+console.log(foodItems)
+console.log(foodItems[0])
+console.log(foodItems[2])
+console.log(foodItems[2])
+foodItems[0]="chicken 65"
+console.log(foodItems)
+foodItems[4]="veg biryani"
+console.log(foodItems)
+foodItems.push("paneer tikka")
+console.log(foodItems)
+foodItems.pop()
+console.log(foodItems)
+foodItems.unshift("chicken tikka")
+console.log(foodItems)
+foodItems.shift()
+console.log(foodItems)
+ foodItems[0]=""
+console.log(foodItems)
+
+
+const fruits=[]
+fruits[0]="apple"
+fruits[1]="melon"
+console.log(fruits)
+fruits.push("orange","banana","pear")
+console.log(fruits)
+fruits.pop()
+fruits.unshift("sapota","cherry")
+fruits.shift()
+console.log(fruits)
