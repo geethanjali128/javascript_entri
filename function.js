3c1dc20902230dca8bf2f22509d6d5391aacb29c
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-03 20:39:05 +0530

    function

---
+++ function.js
@@ -0,0 +1,35 @@
+function add(a,b)
+{let c=a+b;
+    return c;
+}
+let x=add(10,100)
+let y=add(150,150)
+console.log(x,y)
+
+function add1(a,b,c)
+{let d=a+b+c;
+    return d;
+}
+let p=add1(50,50,100)
+console.log(p)
+
+function text(x,y)
+{let z=x-y;
+    return z;
+}
+let a=text(50,25)
+console.log(a)
+
+function put(p,q)
+{let r=p*q;
+    return r;
+}
+let b=put(2,3);console.log(b)
+
+function put(p,q)
+{let r=p/q;
+    return r;
+}
+let c=put(8,4);console.log(c)
+
+
