1482792ac8cd5110a147da7a993b72eb643d5003
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-03 20:40:23 +0530

    for loop

---
+++ for_loop.js
@@ -0,0 +1,34 @@
+let array1=[1,2,3,4,5]
+for ( let i=0;i<=array1.length;i++){
+   console.log(array1[i])
+} 
+
+let array2=[2,3,4,5]
+let total=0
+for (let i=0;i<array2.length;i++){
+   total=total+array2[i]
+  }
+console.log(total)
+
+let array3=[2,3,4,5]
+var total1=0
+for(let i=0;i<array3.length;i++){
+   total1=total1+array3[i]
+}
+console.log(total1)
+
+let array4=[2,3,4,5]
+for(let i=0;i<array3.length;i++){
+   var total2=0
+   total2=total2+array4[i]
+}
+console.log(total2)
+
+let array5=[2,3,4,5]
+for (let i=0;i<array2.length;i++){
+   let total3=0
+   total3=total3+array5[i]
+   console.log(total3)
+  }
+
+
