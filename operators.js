5ce57cdf3fc81e28b97673dfeab80fc6f5752327
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-05 18:16:23 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,68 @@
+//  assignment operator (=) - assign a value to a variable
+let x=10
+let y=20
+//  addition operator(+)-to add values
+let z=x+y
+console.log(z)
+//  substraction operator (-)
+let p=x-y
+console.log(p)
+// multiplication operator(*)
+let q=x*y
+console.log(q)
+//  division operator(/)
+let r=x/y
+console.log(r)
+//  exponential operator(**)
+let s=2
+let u=x**s
+console.log(u)
+//  division remainder(%)-gives remainder
+let v=x%s
+console.log(v)
+
+
+let a=5
+let b=6
+console.log(a,b)
+a+=2
+b-=3
+console.log(a,b,)
+let c=3
+c*=3
+console.log(c)
+
+// // (==) -equal to
+let n1=4
+let n2=4
+console.log(n1==n2)
+
+// (==)-only check equal value
+let n3=4
+let n4="4"
+console.log(n3==n4)
+
+// (===)-check equal value and equal data type
+let fl=4
+let sl=4
+let tl="4"  
+console.log(fl===sl)
+console.log(fl===tl)
+
+// (!=) - check not equal value
+let n5=4
+let n6=4
+console.log(n5!=n6)
+let n7=5
+console.log(n5!=n7)
+let n8="4"
+console.log(n5!=n8)
+
+// (!==)-check not equal value and datatype
+let n9=5
+let n10=6
+let n11="6"
+console.log(n9!==n10)
+console.log(n9!==n11)
+
+
