9c3f3b6ca7a8855b26d2a040b02ab6bdb7af37f8
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-06 17:00:10 +0530

    ternary operator

---
+++ ternary1.js
@@ -0,0 +1,13 @@
+let value
+let a=10
+let b=15
+value=(a>b)?console.log("a"):console.log("b")
+
+let n=60
+ data=n>10?n>50?console.log("yes n is greater than 50"):console.log("yes n is less than 50"):console.log("no")
+
+ let p=19
+ data1=p<12?console.log("hi"):p<16?console.log("hello"):console.log("bye")
+
+ let q=8
+ data2=q<12?q<9?console.log("bed coffee"):console.log("breakfast"):q<16?console.log("gd morning"):console.log("gd evening")
