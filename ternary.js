5339252dd595834be44a4df9df457737a69481c0
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-06 16:30:14 +0530

    ternary

---
+++ ternary.js
@@ -0,0 +1,8 @@
+let value
+let year=2003
+value=(year%100!==0)?console.log("true"):console.log("false")
+
+let data
+let a=1
+let b=5
+data=(a>b)?console.log("a is greater than b"):console.log("b is greater than a")
