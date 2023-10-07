0429e7ae5d9b6e97ec6d988aee9b96686ca51a7a
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-07 18:37:42 +0530

    if condition

---
+++ if_if.js
@@ -0,0 +1,28 @@
+
+// using if condition inside if condition
+let n=9
+let name="suma"
+if(n<10){
+    console.log("n is less than 10")
+    if(name==="anjali"){
+        console.log("name is anjali")
+    }
+}else{
+    console.log("no")
+}
+
+let a="student"
+let roll=55
+let age=20
+if(a==="student"){
+    console.log("student")
+    if(roll<50){
+        console.log("student roll no")
+    }if(age<15){
+        console.log("minor")
+    }
+}else{
+    console.log("not a student")
+}
+
+
