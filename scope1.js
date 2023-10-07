10c4b2b8380c1586306fd0dc0e71bbe6e1962a87
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-07 18:39:21 +0530

    scope

---
+++ scope1.js
@@ -0,0 +1,49 @@
+// reassign with let ,var and const
+let a="rose"
+if(true){
+     a="jasmine"
+     let d="jennie"
+    console.log(a)
+    console.log(d)
+}
+console.log(a)
+console.log(d)
+
+
+var b="rohith"
+if(true){
+     b="kumar"
+    console.log(b)
+}
+console.log(b)
+
+const c="siri"
+if(true){
+     c="somi"
+    console.log(c)
+}
+console.log(c)
+
+// redeclare with let,var and const
+let p="lisa"
+if(true){
+    let p="anajali"
+    console.log(p)
+}
+console.log(p)
+
+var q="amar"
+if(true){
+    var q="sai"
+    console.log(q)
+}
+console.log(q)
+
+const r="prasad"
+if(true){
+    const r="sarath"
+    console.log(r)
+}
+console.log(r)
+
+
