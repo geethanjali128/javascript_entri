c240c38695bf60737ee9405fb2f5bfb99cabfeca
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-11 21:41:56 +0530

    function

---
+++ function1.js
@@ -0,0 +1,33 @@
+const sum1=(p,q)=>{
+    console.log("smile")
+    console.log(p,q)
+    console.log(p+q)
+}
+sum1(12,14)
+
+const result=(a,b)=>{
+    return a-b
+}
+console.log(result(12,8))
+
+const greet=(say)=>{
+    return say
+    
+}
+console.log(greet("hello"))
+const text=greet("hi")
+console.log(text)
+const text1=greet("good morning")
+console.log(text1)
+console.log(text+","+text1)
+
+function greeting(put){
+    return put
+}
+ const data=greeting("hello good morning")
+console.log(data)
+
+
+
+
+
