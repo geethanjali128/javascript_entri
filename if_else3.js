f2d1a7f6f25af7298cabf5d36f28af54ab21a472
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-15 21:42:42 +0530

    if else

---
+++ if_else3.js
@@ -0,0 +1,19 @@
+let show=""
+let val=8
+if(val==0){
+    show="today is monday"
+}else if(val==1){
+    show="today is tuesday"
+}else if(val==2){
+    show="today is wednesday"
+}else if(val==3){
+    show="today is thursday"
+}else if(val==4){
+    show="today is friday"
+}else if(val==5){
+    show="today is saturday"
+}else {
+    show="today is sunday"
+}
+console.log(show)
+    
