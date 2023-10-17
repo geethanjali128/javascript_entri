f943c47fb7708ef35d3640eaa3fb4bda66831a34
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-17 21:47:26 +0530

    if else

---
+++ if_else4.js
@@ -0,0 +1,20 @@
+
+let fname="ramesh"
+let age=25
+if(fname==="ramesh",age<20){
+        console.log("student")
+    } else if(fname==="suresh",age<20){
+        console.log("student") 
+    }else if(fname==="rajesh",age<20){
+        console.log("student") 
+    } else if(fname==="mahi",age<20){
+        console.log("student") 
+    }else if(fname==="kamal",age<20){
+        console.log("student") 
+    }else if (fname==="rohit",age<20){
+        console.log("student") 
+    }else{
+        console.log("teacher")
+    }
+   
+
