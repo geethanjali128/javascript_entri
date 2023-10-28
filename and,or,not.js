3c7c96c90c6bd02ff0686026f924643f74b7fd5b
geethanjali128 <karrageethanjali2003@gmail.com>
2023-10-28 16:48:26 +0530

    operaors

---
+++ and,or,not.js
@@ -0,0 +1,26 @@
+// &&-and operator
+let n="ramesh"
+let age=15
+if(n==="suresh"&&age<20){
+    console.log("student")
+}
+else{
+    console.log("teacher")
+}
+
+// ||- or operator
+let strength=60
+let className=10
+if(strength===50||className===9){
+    console.log("9 th class student")
+}else{
+    console.log("is not 9th class student")
+}
+let n1=16
+if(!15<n1<20){
+    console.log("true")
+}else{
+    console.log("false")
+}
+
+
