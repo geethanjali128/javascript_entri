4bec7b6ba405f91b73f2fcea54fc65124068b7a5
geethanjali128 <karrageethanjali2003@gmail.com>
2023-11-16 22:17:53 +0530

    fpr loop

---
+++ for_loop1.js
@@ -0,0 +1,14 @@
+const names=["anjali","suma","rama","geetha"]
+
+for(let i=0;i<names.length;i++){
+    console.log(names[i])
+}
+
+for(let i=0;i<=5;i++){
+    console.log(i)
+}
+for(let i=5;i=5;i--){
+    console.log(i)
+}
+
+
