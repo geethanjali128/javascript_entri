789b93924220aa4ab772b1341597ce17e34056ca
geethanjali128 <karrageethanjali2003@gmail.com>
2023-12-03 18:36:28 +0530

    file

---
+++ j-script1.js
@@ -0,0 +1,23 @@
+let x
+var y
+const z=15
+
+x=5
+y=10
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+let text="geethanjali"
+text='geethanjali'
+text="geeth'anjali"
+text='geeth"anjali'
+text=`gee'than"jali`
+text='geeth\'anj\'ali'
+text="geeth\"anj\"ali"
+console.log(text)
+console.log(text.length)
+console.log(typeof(text))
+console.log(text+5)
+console.log(typeof(text+5))
