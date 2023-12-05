d91dedc35255b6836be34199ff069f9bf6846017
geethanjali128 <karrageethanjali2003@gmail.com>
2023-12-05 21:33:14 +0530

    javascript

---
+++ j-script3.js
@@ -0,0 +1,27 @@
+// indexOf()
+let text="please answer if you know the answer of this question"
+let index=text.indexOf("answer")
+let index1=text.indexOf("answer",17)
+console.log(index)
+console.log(index1)
+let index2=text.lastIndexOf("locate")
+let index3=text.lastIndexOf("answer")
+console.log(index2)
+console.log(index3)
+
+// 
+let x=10
+x=15
+console.log(x)
+var y=16
+y=45
+console.log(y)
+var y=23
+console.log(y)
+const z=50
+// z=67
+console.log(z)
+// const z=34
+// console.log(z)
+// let x=34
+// console.log(x)
