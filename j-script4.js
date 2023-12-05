620239819eead4c0f006766ecd2555d2ba42ae94
geethanjali128 <karrageethanjali2003@gmail.com>
2023-12-05 22:17:53 +0530

    javascript

---
+++ j-script4.js
@@ -0,0 +1,22 @@
+// array
+const flowers=["rose","jasmine","lotus","lilly"]
+let a=flowers[0]
+console.log(a)
+let b=flowers[2]
+console.log(b)
+// flowers[5]="tulips"
+flowers[4]="tulips"
+console.log(flowers)
+flowers[0]="sunflower"
+console.log(flowers)
+console.log(flowers[flowers.length -1])
+
+const array=[]
+console.log(array)
+array[0]=1
+array[1]=2
+array[2]=3
+console.log(array)
+array[1]=4
+console.log(array)
+console.log(array[array.length -2])
