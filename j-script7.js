f7cea9c6a0f9fa0f01c820904daa0ea13f828d22
geethanjali128 <karrageethanjali2003@gmail.com>
2023-12-07 19:30:16 +0530

    operators

---
+++ j-script7.js
@@ -0,0 +1,21 @@
+console.log("hello")
+
+function greeting(greet){
+    console.log(greet)
+}
+greeting("good morning")
+greeting("hii")
+console.log("welcome")
+greeting("bye")
+
+// operators
+let a=10
+let b=10
+console.log(a==b)
+console.log(a===b)
+let c="10"
+console.log(b==c)
+console.log(b===c)
+// a=a+10
+a+=10
+console.log(a)
