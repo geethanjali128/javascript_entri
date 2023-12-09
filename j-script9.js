8dde12c96a4568289c3e1da8296a8abb684bb7b5
geethanjali128 <karrageethanjali2003@gmail.com>
2023-12-09 22:51:37 +0530

    csope

---
+++ j-script9.js
@@ -0,0 +1,48 @@
+let x="siri"
+const input=()=>{
+    let x="ramu"
+    console.log(x)
+}
+input()
+console.log(x)
+
+let y="rohith"
+const input1=()=>{
+    y="kumar"
+    console.log(y)
+}
+input1()
+console.log(y)
+
+var z="hello"
+const input2=()=>{
+    var z="hii"
+    console.log(z)
+}
+input2()
+console.log(z)
+
+var p="avanigadd"
+const input3=()=>{
+    p="vijayawada"
+    console.log(p)
+}
+input3()
+ console.log(p)
+
+ const q="banana"
+ const input4=()=>{
+    const q="apple"
+    console.log(q)
+ }
+ input4()
+ console.log(q)
+
+ const r="banana"
+ const input5=()=>{
+     r="apple"
+    console.log(r)
+ }
+ input5()
+ console.log(r)
+ 
