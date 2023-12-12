71e1ccfeb6c133d2b0bf63e0aab36c569926d5a7
geethanjali128 <karrageethanjali2003@gmail.com>
2023-12-12 20:15:12 +0530

    loops

---
+++ j-script14.js
@@ -0,0 +1,29 @@
+let i=1
+while(i<=5){
+    console.log("hii")
+    i++
+}
+//  console.log(i)
+j=1
+ while(j<5){
+    console.log(j)
+    j++
+ }
+
+ let count=10
+ do{
+    console.log(count)
+    count--
+ }while(count>0)
+
+ var fruits=['apple','mango','pear','plum']
+ for(let x in fruits){
+    console.log(fruits[x])
+ }
+ for (let ele of fruits){
+   console.log(fruits[ele])
+ }
+
+ for(let i=0;i<fruits.length;i++){
+   console.log(fruits[i])
+ }
