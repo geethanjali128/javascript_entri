7123921003a6369f0b8180287c164030d1ad774c
geethanjali128 <karrageethanjali2003@gmail.com>
2024-02-29 22:45:22 +0530

    javascript problems

---
+++ problems/2.js
@@ -0,0 +1,21 @@
+// **2. Factorial of a Number:**
+// Write a program to calculate the factorial of a given number `n` using a `for` loop.
+
+
+// 1)
+const n=5
+let factorial=1
+for(let i=1;i<=n;i++){
+    factorial*=i
+}
+console.log(factorial)
+
+
+
+// 2)
+const n1=5
+let factorial1=1
+for(let i=n1;i>0&&i<=n1;i--){
+    factorial1*=i
+}
+console.log(factorial1)
