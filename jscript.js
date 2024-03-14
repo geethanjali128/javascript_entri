cba934e57b12d700bd5041162b29c5a97a749645
geethanjali128 <karrageethanjali2003@gmail.com>
2024-03-14 23:47:41 +0530

    hoisting

---
+++ jscript.js
@@ -0,0 +1,21 @@
+
+console.log(a)
+var a
+console.log(a)
+a=10
+console.log(a)
+
+b()
+console.log(b)
+function b(){
+    console.log("b")
+}
+b()
+
+// c()
+console.log(c)
+var c=function(){
+    console.log("c")
+}
+console.log(c)
+c()
