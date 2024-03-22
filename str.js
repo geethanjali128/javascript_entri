366cb6bff7edbe2b43489dbe3b901a54f45b8037
geethanjali128 <karrageethanjali2003@gmail.com>
2024-03-22 22:36:27 +0530

    javascript

---
+++ str.js
@@ -0,0 +1,51 @@
+//        var b=10
+//    function a(){
+
+//     c()
+//     function c(){
+//         console.log(b)
+//     }
+//    }
+//    a()
+
+    // if(true){
+    //     const a=10
+    //     console.log(a)
+    // }
+    // console.log(a)
+
+
+    // const a=[1,2,3,4,5]
+
+    // const map=a.map( val => val*2 )
+
+    // console.log(map)
+   
+
+    // const each=a.forEach( val =>{
+    //     console.log(val*2)
+    // } )
+
+    //  console.log(each)
+
+
+    // const obj={
+    //     0:"a",
+    //     1:"b",
+    //     2:"c",
+    //     length:3
+    // }
+
+
+    // const arr1=Array.from(obj)
+
+    // console.log(arr1)
+
+
+    var x=20
+    function abc(){
+        console.log(x)
+        var x=10
+    }
+
+    abc()
